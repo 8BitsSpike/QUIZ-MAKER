b9f1dfe678ede85d1e6acf966eb612d72e7143df
Pedro de Oliveira Rosas <81116171+8BitsSpike@users.noreply.github.com>
2025-03-21 02:46:09 -0300

    updates
    correções na mecanica de salvar quiz e posicionamento

--- quiz/script.js
+++ quiz/script.js
@@ -2,19 +2,20 @@ function listagem() {
   let listaQuiz = document.getElementById('listaQuiz');
   const coleta = localStorage.getItem('QuizDB');
   const lista = coleta ? JSON.parse(coleta) : 'vazio';
-  let texto;
+  let texto = '';
   if (lista == 'vazio') {
     texto = 'Nenhum Quiz até o momento';
   } else {
-    let arraylista = lista['quizes'];
+    let arraylista = Object.keys(lista);
     for (let k = 0; k < arraylista.length; k++) {
       let qzno = 'quiz' + k;
-      let obj = arraylista[qzno];
+      let pergs = lista[qzno];
+      let obj = pergs.perg0;
       let titulo = obj.titulo;
-      texto += '<div class="tituloQuiz">' + titulo + '</div><br>';
+      texto += '<div class="tituloQuiz">' + titulo + '</div>';
     }
   }
-  listaQuiz.innerHTML = '<div class="boxLista">' + texto + '</div>';
+  listaQuiz.innerHTML += '<div class="boxLista">' + texto + '</div>';
 }
 
 function criar() {
@@ -26,23 +27,23 @@ function criar() {
         <div id="pergExtras">
           <div id="perg0">
             <label for="titu">Titulo do Quiz:</label>
-            <input type="text" id="titu" name="titu" size="74" required><br><br>
+            <input type="text" id="titu" size="74" required><br><br>
 
             <label for="enuncia">1° Questão apresentada pelo Quiz:</label><br>
-            <input type="text" id="enuncia" name="enuncia" size="90" required><br><br>
+            <input type="text" id="enuncia" size="90" required><br><br>
 
             <label for="resp0">1° resposta:</label><br>
-            <input type="text" id="resp0" name="resp" size="90" required><br>
+            <input type="text" id="resp0" size="90" required><br>
 
             <label for="resp1">2° resposta:</label><br>
-            <input type="text" id="resp1" name="resp1" size="90" required><br>
+            <input type="text" id="resp1" size="90" required><br>
 
             <div id="respExtra"></div><br>
 
             <button type="button" id="btnMaisResp" onclick="maisOpc('perg0')">+ respostas</button><br><br>
 
             <label for="correto">Resposta correta:</label><br>
-            <input type="number" id="correto" name="correto" size="3" maxlength="2" required><br><br>
+            <input type="number" id="correto" size="3" maxlength="2" required><br><br>
           </div>
         </div><br><br>
 
@@ -66,15 +67,16 @@ function salvar() {
   for (let k = 0; k < nuperg; k++) {
     let pergObj = {};
     let pergid = 'perg' + k;
-    let perg = crud.querySelector(pergid);
-    let titu = perg.querySelector('titu').value;
-    let ques = perg.querySelector('enuncia').value;
-    let gaba = perg.querySelector('correto').value;
+    let perg = crud.querySelector(`[id='${pergid}']`);
+    console.log(perg, pergid);
+    let titu = perg.querySelector("[id='titu']").value;
+    let ques = perg.querySelector("[id='enuncia']").value;
+    let gaba = perg.querySelector("[id='correto']").value;
     pergObj = { titulo: titu, pergunta: ques, gabarito: gaba };
-    let nresp = perg.querySelector('respExtra').childElementCount + 2;
+    let nresp = perg.querySelector("[id='respExtra']").childElementCount + 2;
     for (let i = 0; i < nresp; i++) {
       let respname = 'resp' + i;
-      let resp = perg.querySelector(respname).value;
+      let resp = perg.querySelector(`[id='${respname}']`).value;
       pergObj[respname] = resp;
     }
     quizData[pergid] = pergObj;
@@ -82,14 +84,16 @@ function salvar() {
   const coleta = localStorage.getItem('QuizDB');
   const lista = coleta ? JSON.parse(coleta) : 'vazio';
   if (lista == 'vazio') {
-    let quiz0 = quizData;
+    let quiz0 = {};
+    quiz0.quiz0 = quizData;
     localStorage.setItem('QuizDB', JSON.stringify(quiz0));
   } else {
-    let nquiz = lista.keys();
+    let nquiz = Object.keys(lista).length;
     let nomequiz = 'quiz' + nquiz;
     lista[nomequiz] = quizData;
     localStorage.setItem('QuizDB', JSON.stringify(lista));
   }
+  listagem();
   let areaCriacao = document.getElementById('areaCriacao1');
   let retornoAoNormal =
     '<button type="button" id="btnCriar" onclick="criar()">Criar Novo Quiz?</button>';
@@ -111,20 +115,20 @@ function maisperg() {
     let idperg = 'perg' + k;
     let pergchild = document.getElementById(idperg);
     if (!pergchild) continue;
-    let perg = pergchild.querySelector("[name='enuncia']").value;
+    let perg = pergchild.querySelector("[id='enuncia']").value;
     arrayperg.push(perg);
-    let er = pergchild.querySelector('#respExtra');
+    let er = pergchild.querySelector("[id='respExtra']");
     let ern = er.childElementCount;
     let volresp = ern + 2;
     let quizobj = {};
     for (let i = 0; i < volresp; i++) {
       let respnome = 'resp' + i;
       let respvalor =
-        pergchild.querySelector(`[name='${respnome}']`)?.value || '';
+        pergchild.querySelector(`[id='${respnome}']`)?.value || '';
       quizobj[respnome] = respvalor;
     }
 
-    let gaba = pergchild.querySelector("[name='correto']").value;
+    let gaba = pergchild.querySelector("[id='correto']").value;
     quizobj['gaba'] = gaba;
     arrayperg.push(quizobj);
   }
@@ -135,15 +139,15 @@ function maisperg() {
       <label for="enuncia">${
         nuperg + 1
       }° Questão apresentada pelo Quiz:</label><br>
-      <input type="text" name="enuncia" size="90" required><br><br>
+      <input type="text" id="enuncia" size="90" required><br><br>
       <label for="resp0">1° resposta:</label><br>
-      <input type="text" name="resp0" size="90" required><br>
+      <input type="text" id="resp0" size="90" required><br>
       <label for="resp1">2° resposta:</label><br>
-      <input type="text" name="resp1" size="90" required><br>
+      <input type="text" id="resp1" size="90" required><br>
       <div id="respExtra"></div><br>
       <button type="button" id="${btnnome}" onclick="maisOpc('${newperg}')">+ respostas</button><br><br>
       <label for="correto">Resposta correta:</label><br>
-      <input type="number" name="correto" size="3" maxlength="2" required><br><br>
+      <input type="number" id="correto" size="3" maxlength="2" required><br><br>
     </div>
   `;
   areaperg.insertAdjacentHTML('beforeend', newQuestionHTML);
@@ -151,31 +155,31 @@ function maisperg() {
     let idperg = 'perg' + j / 2;
     let pergchild = document.getElementById(idperg);
     if (!pergchild) continue;
-    let enunciado = pergchild.querySelector("[name='enuncia']");
+    let enunciado = pergchild.querySelector("[id='enuncia']");
     if (enunciado) enunciado.value = arrayperg[j];
     let er = pergchild.querySelector('#respExtra');
     let ern = er.childElementCount;
     let volresp = ern + 2;
     for (let h = 0; h < volresp; h++) {
       let respnome = 'resp' + h;
-      let respvalor = pergchild.querySelector(`[name='${respnome}']`);
+      let respvalor = pergchild.querySelector(`[id='${respnome}']`);
       if (respvalor) {
         respvalor.value = arrayperg[j + 1][respnome];
       }
     }
-    let gaba = pergchild.querySelector("[name='correto']");
+    let gaba = pergchild.querySelector("[id='correto']");
     if (gaba) gaba.value = arrayperg[j + 1]['gaba'];
   }
 }
 
 function maisOpc(lugar) {
   const parent = document.getElementById(lugar);
-  const area = parent.querySelector('#respExtra');
+  const area = parent.querySelector("[id='respExtra']");
   let filhotes = area.childElementCount;
   let arrayResposta = [];
   for (let k = 0; k < filhotes; k++) {
-    let filhoteAtual = `#resp${k + 2}`;
-    let coleira = area.querySelector(filhoteAtual);
+    let filhoteAtual = `resp${k + 2}`;
+    let coleira = area.querySelector(`[id="${filhoteAtual}]`);
     if (coleira) arrayResposta.push(coleira.value);
   }
   let nome = `resp${filhotes + 2}`;
@@ -183,13 +187,13 @@ function maisOpc(lugar) {
   let conteudo = `
     <div>
       <label for="${nome}">${posi}</label><br>
-      <input type="text" id="${nome}" name="${nome}" size="90" required><br>
+      <input type="text" id="${nome}" size="90" required><br>
     </div>
   `;
   area.insertAdjacentHTML('beforeend', conteudo);
   for (let i = 0; i < filhotes; i++) {
     let filhoteAtual = `#resp${i + 2}`;
-    let coleira = area.querySelector(filhoteAtual);
+    let coleira = area.querySelector(`[id="${filhoteAtual}]`);
     if (coleira) coleira.value = arrayResposta[i];
   }
 }
