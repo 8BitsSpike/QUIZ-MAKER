ed7e433d748c68aad9245cc85f32a8ea75eb58db
Pedro de Oliveira Rosas <81116171+8BitsSpike@users.noreply.github.com>
2025-04-16 23:28:30 -0300

    Update script.js

---
+++ blog/script.js
@@ -0,0 +1,10 @@
+/*
+Precisa ser desenvolvidas as seguintes funções:
+- listagem de postagens existentes
+- abertura da area de edição
+- salvar postagem
+- editar postagem
+- cancelar postagem
+- excluir postagem
+- area de exibção do post
+*/
